2135834bdfc4453a789b6fffa308c6642dfb5e34
DongerinoDude <wa4554wa@163.com>
2018-07-07 13:24:55 +0800

    Add initial py project

---
+++ MainWindow/MainWindow_Business.py
@@ -0,0 +1,6 @@
+from PyQt5 import QtWidgets
+from MainWindow.MG_MainWindow import Ui_MainWindow
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+from MainWindow.MainWindow_Business import MainWindow
+from PyQt5 import QtWidgets
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
